b7c1c1b4d0f8203c8e2ecdea946cebcda41d6d10
Bigmoletos <32773811+bigmoletos@users.noreply.github.com>
2025-05-23 18:25:52 +0200

    Réorganisation des paramètres de build dans le fichier test_integration_airquality.groovy. Les blocs de paramètres ont été restructurés pour améliorer la lisibilité et la maintenabilité de la configuration des jobs CI/CD. Cette mise à jour vise à clarifier les options disponibles pour les utilisateurs et à faciliter la gestion des pipelines.

--- ci-cd/jenkins/casc/jobs/test_integration_airquality.groovy
+++ ci-cd/jenkins/casc/jobs/test_integration_airquality.groovy
@@ -15,47 +15,45 @@ pipelineJob('infrastructure/hebergement/test_integration_airquality') {
     - L'exécution des tests d'intégration
     - La génération des rapports de tests''')
 
-    // Configuration du projet GitHub associé
     properties {
         githubProjectUrl('https://gitlab.com/iaproject-fr/airquality.git')
-        // Paramètres du build
-        parameters {
-            stringParam {
-                name('BRANCH_NAME')
-                defaultValue('main')
-                description('Branche à tester')
-            }
-            stringParam {
-                name('TEST_VOLUME_NAME')
-                defaultValue('integration_tests_vol_${BUILD_TAG}')
-                description('Nom du volume pour les tests')
-            }
-            booleanParam {
-                name('DEBUG_MODE')
-                defaultValue(false)
-                description('Activer le mode debug pour plus de logs')
-            }
-            // Paramètres Docker
-            stringParam {
-                name('DOCKER_REGISTRY')
-                defaultValue('docker.io')
-                description('Registry Docker à utiliser')
-            }
-            stringParam {
-                name('DOCKER_NAMESPACE')
-                defaultValue('${DOCKER_USERNAME}')
-                description('Namespace Docker (votre username)')
-            }
-            stringParam {
-                name('IMAGE_NAME')
-                defaultValue('air_quality_ihm')
-                description('Nom de l\'image Docker')
-            }
-            stringParam {
-                name('IMAGE_TAG')
-                defaultValue('latest')
-                description('Tag de l\'image Docker')
-            }
+    }
+
+    parameters {
+        stringParam {
+            name('BRANCH_NAME')
+            defaultValue('main')
+            description('Branche à tester')
+        }
+        stringParam {
+            name('TEST_VOLUME_NAME')
+            defaultValue('integration_tests_vol_${BUILD_TAG}')
+            description('Nom du volume pour les tests')
+        }
+        booleanParam {
+            name('DEBUG_MODE')
+            defaultValue(false)
+            description('Activer le mode debug pour plus de logs')
+        }
+        stringParam {
+            name('DOCKER_REGISTRY')
+            defaultValue('docker.io')
+            description('Registry Docker à utiliser')
+        }
+        stringParam {
+            name('DOCKER_NAMESPACE')
+            defaultValue('${DOCKER_USERNAME}')
+            description('Namespace Docker (votre username)')
+        }
+        stringParam {
+            name('IMAGE_NAME')
+            defaultValue('air_quality_ihm')
+            description('Nom de l\'image Docker')
+        }
+        stringParam {
+            name('IMAGE_TAG')
+            defaultValue('latest')
+            description('Tag de l\'image Docker')
         }
     }
 
